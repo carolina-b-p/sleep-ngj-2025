d0aba9b3591b6e853983f6b254133de55d0f3d5a
Carolina <carolina.bermejo.pascual@lego.com>
2025-04-05 23:35:05 +0200

    UI bar change color

--- SleepNGJ2025/Assets/Scripts/SleepManager.cs
+++ SleepNGJ2025/Assets/Scripts/SleepManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.PlayerLoop;
 using UnityEngine.Serialization;
@@ -9,6 +10,7 @@ public class SleepManager : MonoBehaviour
 {
     public Image sleepBarLevel;
     public float sleepAmount = 100f;
+    public List<Color> sleepColors = new List<Color>();
     
     //singleton instance
     public static SleepManager Instance { get; private set; }
@@ -33,6 +35,26 @@ public class SleepManager : MonoBehaviour
     public void UpdateSleepBar()
     {
         sleepBarLevel.fillAmount = sleepAmount / 100f;
+        if (sleepAmount / 100 <= 1 && sleepAmount / 100 > 0.75f)
+        {
+            sleepBarLevel.color = sleepColors[1];
+        }
+        else if (sleepAmount / 100 <= 0.75f && sleepAmount / 100 > 0.5f)
+        {
+            sleepBarLevel.color = sleepColors[2];
+        }
+        else if (sleepAmount / 100 <= 0.5f && sleepAmount / 100 > 0.25f)
+        {
+            sleepBarLevel.color = sleepColors[3];
+        }
+        else if (sleepAmount / 100 <= 0.25f && sleepAmount / 100 > 0.1f)
+        {
+            sleepBarLevel.color = sleepColors[4];
+        }
+        else if (sleepAmount / 100 <= 0.1f)
+        {
+            sleepBarLevel.color = sleepColors[5];
+        }
     }
 
 }
