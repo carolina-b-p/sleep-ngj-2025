cefdb7b28542235cf44ae1f4460e87e74db0c8a6
Magnus Nielsen <magnus.nielsen@gmail.com>
2025-04-06 14:09:29 +0200

    main menu edits (credits)

--- SleepNGJ2025/Assets/GOTOSCENENUMBER.cs
+++ SleepNGJ2025/Assets/GOTOSCENENUMBER.cs
@@ -6,6 +6,8 @@ public class GOTOSCENENUMBER : MonoBehaviour
 {
     public int sceneNumber; // The scene number to load
     // Start is called before the first frame update
+    public GameObject startButton, quitButton, returnToMM, CreditsButton, movedCreditsPos, creditsStartPos;
+
     public void GoTo()
     {
         // Load the scene with the specified scene number
@@ -16,5 +18,23 @@ public class GOTOSCENENUMBER : MonoBehaviour
     {
         Application.Quit();
         print("quit");
+    } 
+
+    public void OpenCredit()
+    {
+        startButton.SetActive(false);
+        quitButton.SetActive(false);
+        returnToMM.SetActive(true);
+        CreditsButton.transform.position = movedCreditsPos.transform.position;
+        print("Show credits");
+    }
+
+    public void returnToMainMenu()
+    {
+        startButton.SetActive(true);
+        quitButton.SetActive(true);
+        returnToMM.SetActive(false);
+        CreditsButton.transform.position = creditsStartPos.transform.position;
+
     }
 }
