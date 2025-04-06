ee15dcdd7b40d9d30ed56c5f2189df0a075e410e
Alfenhose <mikkel@balslev.info>
2025-04-06 14:42:22 +0200

    sleep overlay prefab

---
+++ SleepNGJ2025/Assets/sleepoverlayscript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sleepoverlayscript : MonoBehaviour
+{
+    //i have a canvasgroup and want to update the alpa by lerping towards the target alpha
+    public CanvasGroup canvasGroup;
+    public CarController carController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        carController = FindObjectOfType<CarController>();    
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0f; // Set initial alpha to 0
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var targetAlpha = carController.isSleeping ? 1f : 0f;
+        canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, targetAlpha, 0.1f);
+    }
+}
